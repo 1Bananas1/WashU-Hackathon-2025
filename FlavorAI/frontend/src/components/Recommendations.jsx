5de1bf5a781edf0044feb856ab603bf2096ec7f8
Rojoloco04 <parrackjack@gmail.com>
2025-04-13 12:10:19 -0500

    clean up and update requirements

--- FlavorAI/frontend/src/components/Recommendations.jsx
+++ FlavorAI/frontend/src/components/Recommendations.jsx
@@ -29,8 +29,8 @@ const Recommendations = () => {
       try {
         // We include 'n' in our POST body so the server can return exactly that many results
         const requestBody = {
-          lat: 47.4979,
-          lon: 19.0402,
+          lat: 38.6270,
+          lon: -90.1994,
           radius_value: radius,
           radius_unit: "miles",
           triedFoods: [],
